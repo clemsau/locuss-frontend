2633fc825e30a06ab865a3507356fa06040b00c5
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-10-17 20:45:55 +0200

    Add nuxt-auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        locuss-frontend
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div v-if="$store.state.auth">
+      <p>
+        You are authenticated. You can see the
+        <NuxtLink to="/secret">
+          secret page
+        </NuxtLink>!
+      </p>
+      <button @click="logout">
+        Logout
+      </button>
     </div>
+    <p v-else>
+      Please
+      <NuxtLink to="/login">
+        login
+      </NuxtLink>.
+    </p>
   </div>
 </template>
 
 <script>
-export default {}
+const Cookie = process.client ? require('js-cookie') : undefined
+
+export default {
+  methods: {
+    logout () {
+      Cookie.remove('auth')
+      this.$store.commit('setAuth', null)
+    }
+  }
+}
 </script>
 
 <style>
